76106c1ffbc132732f6c1c3a5edbb2cb65395cd1
kadamSnehal <snehal9160@gmail.com>
2022-07-27 16:51:50 +0530

    updating repositories

---
+++ server.js
@@ -0,0 +1,19 @@
+var express=require('express')
+var app=express();
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome !!!!</h1>"
+    +"<hr/>"
+    +"<h3>Flowers</h3>"
+    +"<br/>"
+    +"<ol>"
+    +"<h4>"
+    +"<li>Rose</li>"
+    +"<li>Lotus</li>"
+    +"<li>Champa</li>"
+    +"<li>Chameli</li></h4>"
+    +"</ol>")
+});
+app.get("/aboutus",(req,res)=>{
+    res.send("<h1>Nothing to display !!</h1>")
+});
